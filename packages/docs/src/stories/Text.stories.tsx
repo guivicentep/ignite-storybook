4f30d57b545ea0cc90bbae5bb5c939f10140da44
guivicentep <guivicente.gvp@hotmail.com>
2024-01-29 14:12:21 -0300

    feat: Criando repositório para o storybook

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@gvp-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum magni necessitatibus modi rem nihil saepe molestiae ab qui, veritatis quis quod autem temporibus. Itaque eos, officiis libero fugiat voluptates molestias?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
